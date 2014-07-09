ffb93ce3cdd18b965c679f0b2e580adcd737eee5
SlayerBirden <olegkulik1985@gmail.com>
2014-07-09 20:27:30 +0300

    Initial commit

---
+++ server.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg
+ * Date: 7/9/14
+ * Time: 7:10 PM
+ */
+namespace Frp;
+
+class Server
+{
+
+    /**
+     * @param string $query
+     * @return string
+     */
+    public function sendResponse($query)
+    {
+        return json_encode([$query]);
+    }
+}
+$server = new Server();
+$query = $_GET['q'];
+echo $server->sendResponse($query);
